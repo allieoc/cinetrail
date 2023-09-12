cb7ce74e25aec740ee2101a27c1debd6d0a7b5ec
allieoc <allieocon@gmail.com>
2023-09-11 22:02:05 -0400

    popular movies and top rated movies components complete

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import './MovieCard.css'
+import { Link } from 'react-router-dom'
+import Rating from '../Rating/Rating'
+
+function MovieCard({width, height, movie, radius, cardStyle, imageUrl}) {
+
+    const [rating, setRating] = useState(Math.round(movie?.vote_average / 2));
+    
+    const imageStyle = {
+        width: width,
+        height: height,
+        backgroundImage: `url("https://image.tmdb.org/t/p/w500/${imageUrl}")`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "relative",
+        borderRadius: radius,
+        boxShadow: 
+            cardStyle === "popular-card" && "0px 0px 10px 0px rgba(118,118,118,0.75"
+    }
+
+  return (
+    <Link to={`/moviedetails/${movie?.id}`} className={cardStyle} key={movie?.id}> 
+        <div style={imageStyle}>
+            <div className="movie-info-top">
+                <Rating movieRating={rating} />
+            </div>
+            <div className="movie-info-bottom">
+                <p>{movie?.title}</p>
+                <p>Rating: {rating}</p>
+            </div>
+        </div>
+        {cardStyle === "top-rated-card" && <p>{movie?.title}</p>}
+    </Link>
+
+   
+  )
+}
+
+export default MovieCard    
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,20 +2,34 @@ import React, {useEffect, useState} from 'react'
 import './Homepage.css'
 import Slider from '../../components/Slider/Slider'
 import axios from 'axios'
+import MovieCard from '../../components/MovieCard/MovieCard'
 
 function Homepage({ apiKey, baseUrl }) {
 
   const [popularMovies, setPopularMovies] = useState([]);
-  const [topRatedMovies, setTopRatedMovies] = useState([])
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   useEffect(()=>{
     const fetchMovies = async () => {
       try{
         const popularMoviesResponse = await axios.get(
-          `${baseUrl}/movie/popular?api_key=${apiKey}`)
+          `${baseUrl}/movie/popular?api_key=${apiKey}&page=${page}`
+          );
          // console.log(popularMoviesResponse.data.results)
           setPopularMovies(popularMoviesResponse.data.results)
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    fetchMovies();
+  }, [page]);
 
+  useEffect(()=>{
+    const fetchMovies = async () => {
+      try{
           const topRatedMoviesResponse = await axios.get(
             `${baseUrl}/movie/top_rated?api_key=${apiKey}`
           )
@@ -26,7 +40,12 @@ function Homepage({ apiKey, baseUrl }) {
       }
     }
     fetchMovies();
-  })
+  }, [])
+
+  const handlePage = page => {
+    setPage(page);
+    scrollTo({top: 550, left: 0, behavior: "smooth"})
+  }
 
   return (
     <div className="homepage-container">
@@ -37,19 +56,46 @@ function Homepage({ apiKey, baseUrl }) {
           <div className="popular-cards-wrapper">
             {
               popularMovies.map(movie => {
-                return <p key={movie?.id}>{movie?.title}</p>
+                return <MovieCard 
+                height="300px" 
+                width="200px"
+                movie={movie}
+                imageUrl={movie?.poster_path}
+                radius="16px"
+                cardStyle="popular-card" 
+                key={movie?.id}
+                />
               })
             }
+          </div>
+          <div className="page-numbers">
+            <p>Select Page</p>
+            {pageNumbers.map(item => (
+              <p className={item === page ? "current-page":"page"} 
+              key={item}
+              onClick={()=>handlePage(item)}
+              >
+                {item}
+              </p>
+            ))}
           </div>
         </div>
         <div className="top-rated-container">
           <h3>Top Rated Movies</h3>
           <div className="top-rated-cards-wrapper">
-            {
-              topRatedMovies.map(movie => {
-                return <p key={movie?.id}>{movie?.title}</p>
-              })
-            }
+            {topRatedMovies.map((movie) => {
+              return (
+                <MovieCard
+                  height="100px"
+                  width="200px"
+                  movie={movie}
+                  imageUrl={movie?.backdrop_path}
+                  cardStyle="top-rated-card"
+                  key={movie?.id}
+                  radius="8px"
+                />
+              );
+            })}
           </div>
         </div>
       </div>
